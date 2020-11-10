76e86dd13a3e75de74872465853a71afe424ace0
danielsn46 <danielsn46@gmail.com>
2020-11-10 16:23:15 +0100

    3 TABLA

--- Prácticas/p-iaw-1/ejercicio3.php
+++ Prácticas/p-iaw-1/ejercicio3.php
@@ -4,7 +4,16 @@
     </head>
     <body>
         <?php
-            date("F j, Y, g:i a");
+            echo "<table border='1px solid'>";
+            echo "<tr>";
+            echo "<td><b><center>Fecha</center></b></td>";
+            echo "<td><b><center>Hora</center></b></td>";
+            echo "</tr>";
+            echo "<tr>";
+            echo "<td>".date('Y F j')."</td>";
+            echo "<td>".date('G:i a')."</td>";
+            echo "<tr>";
+            echo "</table>";
             // Imprimiremos Mes Día, Año, hora y pm/am
 
         ?>
